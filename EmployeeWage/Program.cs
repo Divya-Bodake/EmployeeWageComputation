42b2ae4c7227ba378f415f1c7dae6105e8d6ba8a
Divya-Bodake <divyabodake25@gmail.com>
2022-11-10 23:47:32 +0530

    [Divya]Add:Check Present or Absent

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee wage computation problem");
+            Employee employee = new Employee();
+            employee.Attendance();
         }
     }
 }
