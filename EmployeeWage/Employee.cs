d71aeac43f9f9f860cdcb48c4fa5bea1bf1ecd98
Divya-Bodake <divyabodake25@gmail.com>
2022-11-11 15:41:32 +0530

    [Divya]Add:Monthly wage

--- EmployeeWage/Employee.cs
+++ EmployeeWage/Employee.cs
@@ -8,14 +8,15 @@ namespace EmployeeWage
 {
     internal class Employee
     {
-        const int IS_FULL_TIME = 0, WAGE_PER_HR = 20, FULL_TIME_HR = 8, PART_TIME_HR = 8, PART_TIME = 1;
+        const int IS_FULL_TIME = 0, WAGE_PER_HR = 20, FULL_TIME_HR = 8, PART_TIME_HR = 8, PART_TIME = 1, ABSENT=2;
         public void CalculateEmpWage()
         {
             int emphr = 0;
             int Empwage = 0;
+            int totalempwage = 0;
 
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
 
             switch (empcheck)
             {
@@ -27,12 +28,21 @@ namespace EmployeeWage
                     Console.WriteLine("Employee is PartTime");
                     emphr = 8;
                     break;
-                
+
+                case ABSENT:
+                    Console.WriteLine("Employee is Absent");
+                    emphr = 0;
+                    break;
+                default:
+                    break;
+
             }
 
             Empwage = emphr * WAGE_PER_HR;
+            totalempwage += Empwage;
             Console.WriteLine(Empwage);
-
+            Console.WriteLine("Total monthly wage is :" +totalempwage);
         }
+       
     }
 }
