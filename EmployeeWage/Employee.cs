7f3bf8ad3b7318fd125aab252e8a34a57709e696
Divya-Bodake <divyabodake25@gmail.com>
2022-11-12 17:42:27 +0530

    [Divya]Add:Total Days Reached for month

--- EmployeeWage/Employee.cs
+++ EmployeeWage/Employee.cs
@@ -8,41 +8,30 @@ namespace EmployeeWage
 {
     internal class Employee
     {
-        const int IS_FULL_TIME = 0, WAGE_PER_HR = 20, FULL_TIME_HR = 8, PART_TIME_HR = 8, PART_TIME = 1, ABSENT=2;
+        const int IS_FULL_TIME = 0, WAGE_PER_HR = 20, FULL_TIME_HR = 8, PART_TIME_HR = 8, PART_TIME = 1, ABSENT=2, WORKING_DAYS = 20 ;
+        int emphr = 0;
+        int Empwage = 0;
+        int totalempwage = 0;
         public void CalculateEmpWage()
         {
-            int emphr = 0;
-            int Empwage = 0;
-            int totalempwage = 0;
-
             Random random = new Random();
-            int empcheck = random.Next(0, 3);
-
-            switch (empcheck)
+            for (int i = 0; i < WORKING_DAYS && emphr < 100; i++)
             {
-                case IS_FULL_TIME:
-                    Console.WriteLine("Employee is FullTime");
-                    emphr = 8;
-                    break;
-                case PART_TIME:
-                    Console.WriteLine("Employee is PartTime");
-                    emphr = 8;
-                    break;
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        emphr += FULL_TIME_HR;
+                        break;
+                    case PART_TIME:
+                        emphr += PART_TIME_HR;
+                        break;
 
-                case ABSENT:
-                    Console.WriteLine("Employee is Absent");
-                    emphr = 0;
-                    break;
-                default:
-                    break;
-
+                }
             }
 
-            Empwage = emphr * WAGE_PER_HR;
-            totalempwage += Empwage;
-            Console.WriteLine(Empwage);
-            Console.WriteLine("Total monthly wage is :" +totalempwage);
+            totalempwage = emphr * WAGE_PER_HR;
+            Console.WriteLine("Monthly salary is:" +totalempwage);
         }
-       
     }
 }
